a2d83d95921a70b3e541b9adc83a2fa30fca9e05
Ahmet Zamanis <ahmetzamanis@gmail.com>
2022-10-11 19:22:10 +0300

    Tune XGB eta

--- WorkingScript.R
+++ WorkingScript.R
@@ -9,6 +9,7 @@ library(embed)
 library(mlr3verse)
 library(mlr3hyperband)
 library(progressr)
+library(xgboost)
 
 options(scipen=999)
 
@@ -434,7 +435,7 @@ term_none <- trm("none")
 #Bayes learner
 learner_bayes <- lrn("classif.naive_bayes",
                      predict_type="prob")
-
+#note: the nominal encoding may impact the performance
 
 
 
@@ -478,6 +479,7 @@ learner_glmnet <- lrn("classif.glmnet",
 
 ###kNN ####
 
+#trafo function
 k_square_odd <- function(k){
   k <- 2^k + 1
   k
@@ -552,3 +554,414 @@ learner_knn <- lrn("classif.kknn",
 
 
 ###SVM ####
+
+#trafo functions
+k_square <- function(k) {
+  k <- 2^k
+  k
+}
+
+k_power10 <- function(k) {
+  k <- 10^k
+  k
+}
+
+#named class weights vector
+svm_weight <- c(1, minority_weight)
+names(svm_weight) <- c("No", "Yes")
+
+
+#tuning space
+learner_svm1 <- lrn("classif.svm",
+                    type="C-classification",
+                    class.weights=svm_weight,
+                    predict_type="prob",
+                    kernel="radial",
+                    scale=FALSE,
+                    cost=to_tune(p_int(1, 6, trafo=k_power10, tags="budget")),
+                    gamma=to_tune(p_int(-8, 2, trafo=k_square)))
+
+
+
+#tuning instance
+tune_svm1 = TuningInstanceSingleCrit$new(task_train, learner_svm1, resample_cv, 
+                                         measure_log, term_none,
+                                         store_benchmark_result = TRUE)
+
+#tune glmnet
+tune_hyper2$initialize()
+set.seed(1923)
+with_progress(tune_hyper2$optimize(tune_svm1))
+
+#results
+tuneres_svm1 <- as.data.table(tune_svm1$archive)
+#cost 1000, gamma 4, logloss 0.3713911
+#try costs around 1000, gamma>4
+
+
+
+
+#tuning space 2, C around 1000, gamma>4
+k_150fold <- function(k) {
+  k <- k*150
+  k
+}
+
+
+learner_svm2 <- lrn("classif.svm",
+                    type="C-classification",
+                    class.weights=svm_weight,
+                    predict_type="prob",
+                    kernel="radial",
+                    scale=FALSE,
+                    cost=to_tune(p_int(1, 10, trafo=k_150fold, tags="budget")),
+                    gamma=to_tune(p_dbl(4, 8)))
+
+
+
+#tuning instance
+tune_svm2 = TuningInstanceSingleCrit$new(task_train, learner_svm2, resample_cv, 
+                                         measure_log, term_none,
+                                         store_benchmark_result = TRUE)
+
+#tune glmnet
+tune_hyper2$initialize()
+set.seed(1923)
+with_progress(tune_hyper2$optimize(tune_svm2))
+
+#results
+tuneres_svm2 <- as.data.table(tune_svm2$archive)
+#cost 750 gamma 4.916431 logloss 0.3718737
+
+
+
+
+#learner with best parameters
+learner_svm <- lrn("classif.svm",
+                    type="C-classification",
+                    class.weights=svm_weight,
+                    predict_type="prob",
+                    kernel="radial",
+                    scale=FALSE,
+                    cost=750,
+                    gamma=4.916431)
+
+
+
+
+
+
+
+###XGBoost ####
+
+
+####tune eta, max_delta_step ####
+
+#tuning space
+learner_xgb1 <- lrn("classif.xgboost",
+                   booster="gbtree",
+                   predict_type="prob",
+                   nthread=3,
+                   nrounds=5000,
+                   early_stopping_rounds=50,
+                   max_delta_step=to_tune(p_int(1,10, tags="budget")),
+                   eta=to_tune(p_dbl(0.1, 0.3)))
+
+
+
+#tuning instance
+tune_xgb1 = TuningInstanceSingleCrit$new(task_train, learner_xgb1, resample_cv, 
+                                         measure_log, term_none,
+                                         store_benchmark_result = TRUE)
+
+#tune xgboost1
+tune_hyper2$initialize()
+set.seed(1923)
+with_progress(tune_hyper2$optimize(tune_xgb1))
+
+#results
+tuneres_xgb1 <- as.data.table(tune_xgb1$archive)
+#eta 0.1081076, max_delta_step 1, logloss 0.6996111
+#bigger max delta steps are worse
+
+
+
+
+#check against max_delta_step = 0
+
+#tuning space
+learner_xgb1.1 <- learner_xgb1
+learner_xgb1.1$param_set$values$max_delta_step <- 0
+
+
+
+#tuning instance
+tune_xgb1.1 = TuningInstanceSingleCrit$new(task_train, learner_xgb1.1, resample_cv, 
+                                         measure_log, term10,
+                                         store_benchmark_result = TRUE)
+
+#tune xgboost1.1
+tune_anneal$initialize()
+set.seed(1923)
+with_progress(tune_anneal$optimize(tune_xgb1.1))
+
+#results
+tuneres_xgb1.1 <- as.data.table(tune_xgb1.1$archive)
+#eta 0.1, max_delta_step 0, logloss 0.6789039
+
+
+
+
+
+####tune tree complexity ####
+
+
+#tuning space
+k_twofold <- function(k){
+  k <- k*2
+  k
+}
+
+learner_xgb2 <- learner_xgb1
+learner_xgb2$param_set$values$max_delta_step <- 0
+learner_xgb2$param_set$values$eta <- 0.1
+learner_xgb2$param_set$values$max_depth <- to_tune(p_int(1, 6, 
+                                                         trafo=k_twofold))
+learner_xgb2$param_set$values$min_child_weight <- to_tune(p_int(1, 10,
+                                                                tags="budget"))
+
+
+
+#tuning instance
+tune_xgb2 = TuningInstanceSingleCrit$new(task_train, learner_xgb2, resample_cv, 
+                                         measure_log, term_none,
+                                         store_benchmark_result = TRUE)
+
+#tune xgboost2
+tune_hyper2$initialize()
+set.seed(1923)
+with_progress(tune_hyper2$optimize(tune_xgb2))
+
+#results
+tuneres_xgb2<- as.data.table(tune_xgb2$archive)
+#12 max_depth, 1 min_child_weight, logloss 0.6404987
+#12 max_depth, 2 min_child_weight, logloss 0.6087693
+#12 max depth, 5 min_child_weight, logloss 0.5768753
+  #very close to max_depth 10's performance. don't try higher max_depth?
+#best tune: max depth 12, min child weight 10, logloss 0.5579934
+  #try higher
+
+
+
+####tune regularization ####
+
+
+#tuning space
+learner_xgb3 <- learner_xgb2
+learner_xgb3$param_set$values$max_depth <- #???
+learner_xgb3$param_set$values$min_child_weight <- #???
+learner_xgb3$param_set$values$gamma <- to_tune(p_int(0, 1))
+learner_xgb3$param_set$values$lambda <- to_tune(p_dbl(0, 2))
+learner_xgb3$param_set$values$alpha <- to_tune(p_dbl(0, 1))
+
+
+
+#tuning instance
+tune_xgb3 = TuningInstanceSingleCrit$new(task_train, learner_xgb3, resample_cv, 
+                                         measure_log, term100,
+                                         store_benchmark_result = TRUE)
+
+#tune xgboost3
+tune_anneal$initialize()
+set.seed(1923)
+with_progress(tune_anneal$optimize(tune_xgb3))
+
+#results
+tuneres_xgb3 <- as.data.table(tune_xgb3$archive)
+
+
+
+
+
+####tune randomization ####
+
+
+#tuning space
+learner_xgb4 <- learner_xgb3
+learner_xgb4$param_set$values$gamma <- #???
+learner_xgb4$param_set$values$lambda <- #???
+learner_xgb4$param_set$values$alpha <- #???
+learner_xgb4$param_set$values$subsample <- to_tune(p_dbl(0.5, 1))
+learner_xgb4$param_set$values$colsample_bytree <- to_tune(p_dbl(0.5, 1))
+
+
+
+#tuning instance
+tune_xgb4 = TuningInstanceSingleCrit$new(task_train, learner_xgb4, resample_cv, 
+                                         measure_log, term100,
+                                         store_benchmark_result = TRUE)
+
+#tune xgboost4
+tune_anneal$initialize()
+set.seed(1923)
+with_progress(tune_anneal$optimize(tune_xgb4))
+
+#results
+tuneres_xgb4 <- as.data.table(tune_xgb4$archive)
+
+
+
+
+#### retune eta ####
+
+#if necessary
+
+
+
+
+
+#### tune nrounds ####
+
+
+#create weights vector
+xgb_weights <- c()
+
+for (i in 1:nrow(df_train)) {
+  if (df_train[i,1] == "Yes") {
+    xgb_weights <- append(xgb_weights, minority_weight)
+  } else {
+    xgb_weights <- append(xgb_weights, 1)
+  }
+}
+
+
+#create XGB data matrix
+xgb_train <- xgb.DMatrix(data=as.matrix(train_x), label=train_y,
+                         weight=xgb_weights)
+
+
+#create xgb params list
+xgb_params <- list (
+  booster="gbtree",
+  objective="binary:logistic",
+  nthread=3,
+  max_delta_step=,
+  eta=,
+  max_depth=,
+  min_child_weight=,
+  subsample=,
+  colsample_bytree=,
+  gamma=,
+  lambda=,
+  alpha=,
+)
+
+
+#run xgb.cv
+set.seed(1923)
+xgb_cv <- xgb.cv(xgb_params, xgb_data, nfold=5, verbose=TRUE, nrounds=?###,
+                   early_stopping_rounds = 50)
+
+
+
+
+#final learner with best tune
+learner_xgb <- learner_xgb4
+learner_xgb$param_set$values$subsample <- #???
+learner_xgb$param_set$values$colsample_bytree <- #???
+learner_xgb$param_set$values$nrounds <- #???
+
+
+  
+  
+  
+  
+## Benchmarking ####
+
+
+### Perform benchmarking ####
+
+#create baseline learner that predicts the class prob distributions
+learner_baseline <- lrn("classif.featureless",
+                        predict_type="prob",
+                        method="weighted.sample")
+
+
+#create benchmark grid
+benchmark_test = benchmark_grid(tasks=task_test,
+                                   learn=list(learner_baseline, learner_bayes, 
+                                              learner_glmnet,
+                                              learner_knn, learner_svm,
+                                              learner_xgb),
+                                   resamplings=resample_cv)
+
+
+
+#perform benchmarking
+set.seed(1923)
+benchmarkres = benchmark(benchmark_test, store_models=TRUE)
+
+
+#save average benchmarking results
+benchmarkres_table <- benchmarkres$aggregate(measures_list)
+
+
+
+
+### Plots and tables ####
+
+
+#prc curves
+prc_plot <- mlr3viz::autoplot(benchmarkres, type="prc")
+prc_plot <- prc_plot + 
+  geom_ribbon(aes(x=x, y=y, ymin=0, ymax=y, fill=modname), alpha=0.075) +
+  labs(x="Recall", y="Precision",
+       title="Precision-recall curves of loan client classifiers",
+       subtitle="Featureless classifier predicts the class proportions as the probability of each class",
+       color="Classifiers",
+       fill="Classifiers") + 
+  theme(legend.position = "right") + 
+  scale_color_brewer(palette="Dark2", labels=c("Featureless", "NaiveBayes", "glmnet", "kNN", "SVM", "XGBoost")) + 
+  scale_fill_brewer(palette="Dark2", labels=c("Featureless", "NaiveBayes", "glmnet", "kNN", "SVM", "XGBoost")) +
+  theme_bw()
+
+
+#line size
+prc_plot$layers[[1]]$aes_params$size=0.75
+
+prc_plot
+
+
+
+
+#table of metrics
+df_metrics <- benchmarkres_table[,c(4, 7:12)]
+colnames(df_metrics) <- c("Classifier", "Recall", "PRAUC",
+                              "Brier score", "Cohen's kappa", "Accuracy", "Time")
+df_metrics[1:6, 1] <- c("Featureless", "NaiveBayes", "glmnet", "kNN", "SVM", "XGBoost")
+df_metrics[,2:6] <- round(df_metrics[,2:6], 4)
+
+
+tb_perf <- gt(data=df_metrics, rowname_col = 1) %>% 
+  tab_header(title="Performance metrics of loan client classifiers",
+             subtitle="Tested on previously unseen data. Threshold probability=0.5") %>% 
+  opt_table_font(font=list(google_font("Calibri"), default_fonts())) %>% 
+  cols_align(align = "center", columns = everything()) %>% 
+  tab_style(locations=cells_column_labels(columns=everything()), 
+            style=list(
+              cell_borders(sides="bottom", weight=px(3)), 
+              cell_text(weight="bold"))) %>% 
+  data_color(columns=c(2,3,5,6), colors=scales::col_numeric(
+    palette=c("red", "green"),
+    domain=c(0,1)
+  )) %>%
+  data_color(columns=c(4), colors=scales::col_numeric(
+    palette=c("green", "red"),
+    domain=c(0,2)
+    )) %>%
+      data_color(columns=c(7), colors=scales::col_numeric(
+        palette=c("green", "red"),
+        domain=c(0,max(df_metrics[,7]))
+        ))
+
+tb_perf
